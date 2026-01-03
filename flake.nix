81062c822aabecbe4152b9a273546f25ef333398
eleocraft <eliseo.cailloux@gmx.de>
2026-01-03 16:44:41 +0100

    fun with macros

--- flake.nix
+++ flake.nix
@@ -22,12 +22,10 @@
         devShells.default =
         let
           rust = pkgs.fenix.latest.toolchain;
-          rust_analyzer = pkgs.fenix.rust-analyzer;
         in
         pkgs.mkShell {
           buildInputs = [
             rust
-            rust_analyzer
           ];
         };
       }
--- src/lib.rs
+++ src/lib.rs
@@ -42,52 +42,38 @@ pub async fn run(config: GSTConfig) -> Result<(), GSTError> {
     let mut low_rate_telemetry = LowRateTelemetry::new();
     let mut mid_rate_telemetry = MidRateTelemetry::new();
 
+    macro_rules! parse_beacon {
+        ($data: ident, $beacon:ident, ($($field:ident),*)) => {
+            match $beacon.from_bytes($data, &mut crc_ccitt) {
+                Ok(()) => {
+                    println!("parsed {}", stringify!($beacon));
+                    $(
+                        println!("{} > {}: {}", stringify!($beacon), stringify!($field), $beacon.$field);
+                    )*
+                    let serialized_telem = $beacon.serialize();
+                    for (address, bytes) in serialized_telem {
+                        nats_client.publish(address, bytes.into()).await.unwrap();
+                    }
+                }
+                Err(e) => {
+                    match e {
+                        ParseError::WrongId => (),
+                        ParseError::BadCRC => eprintln!("{} with bad crc received", stringify!($beacon)),
+                        ParseError::OutOfMemory => eprintln!("{} could not be parsed: not enough bytes", stringify!($beacon)),
+                    }
+                }
+            }
+        };
+    }
+
     loop {
         match lst_receiver.receive().await {
             Ok(msg) => {
                 match msg {
                     lst_receiver::LSTMessage::Relay(data) => {
                         println!("data: {}", data.len());
-                        match low_rate_telemetry.from_bytes(data, &mut crc_ccitt) {
-                            Ok(()) => {
-                                println!("parsed low rate telem");
-                                println!("lst uptime: {}", low_rate_telemetry.uptime);
-                                println!("lst lqi: {}", low_rate_telemetry.lqi);
-                                println!("lst rssi: {}", low_rate_telemetry.rssi);
-                                println!("lst send: {}", low_rate_telemetry.packets_send);
-                                println!("lst good: {}", low_rate_telemetry.packets_good);
-                                let serialized_telem = low_rate_telemetry.serialize();
-                                for (address, bytes) in serialized_telem {
-                                    nats_client.publish(address, bytes.into()).await.unwrap();
-                                }
-                            }
-                            Err(e) => {
-                                match e {
-                                    ParseError::WrongId => (),
-                                    ParseError::BadCRC => eprintln!("low rate telemetry message with bad crc received"),
-                                    ParseError::OutOfMemory => eprintln!("low rate telemetry message could not be parsed: not enough bytes"),
-                                }
-                            }
-                        }
-
-                        match mid_rate_telemetry.from_bytes(data, &mut crc_ccitt) {
-                            Ok(()) => {
-                                println!("parsed mid rate telem");
-                                println!("bat 1 voltage: {}", mid_rate_telemetry.bat1_voltage);
-                                println!("internal temp: {}", mid_rate_telemetry.internal_temperature);
-                                let serialized_telem = mid_rate_telemetry.serialize();
-                                for (address, bytes) in serialized_telem {
-                                    nats_client.publish(address, bytes.into()).await.unwrap();
-                                }
-                            }
-                            Err(e) => {
-                                match e {
-                                    ParseError::WrongId => (),
-                                    ParseError::BadCRC => eprintln!("mid rate telemetry message with bad crc received"),
-                                    ParseError::OutOfMemory => eprintln!("mid rate telemetry message could not be parsed: not enough bytes"),
-                                }
-                            }
-                        }
+                        parse_beacon!(data, low_rate_telemetry, (uptime, rssi, packets_good));
+                        parse_beacon!(data, mid_rate_telemetry, (bat1_voltage));
                     },
                     _ => () // Ignore internal messages for now
                 }
